6059c300490bdd2a1487f9dad33f4dbad4e5597c
Suzana Serboi <smilea@ucsc.edu>
2023-06-11 13:07:48 -0700

    modified app.py, created templates/index.html

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
 
+import random
+import datetime
+
+today = datetime.date.today()
+year = today.year
+
+AUTHOR = "Suzana Serboi"
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    random_number = random.randint(1,10)
+    return render_template("index.html", rand_num=random_number, cur_year=year, my_name=AUTHOR)
 
 
 if __name__ == "__main__":

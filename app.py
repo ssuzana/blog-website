d02586ad7e7a1053b59bc9eacbc56ce466d2f7d8
Suzana Serboi <smilea@ucsc.edu>
2023-06-11 17:45:28 -0700

    create blog.html, update index.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import requests
 import random
 import datetime
 
@@ -16,6 +16,12 @@ def home():
     random_number = random.randint(1,10)
     return render_template("index.html", rand_num=random_number, cur_year=year, my_name=AUTHOR)
 
+@app.route('/blog')
+def blog():
+    blog_url = "https://api.npoint.io/93d4fda66d6ecc6f55dd"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
